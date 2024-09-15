4b3733729ab6222e185c00094e4cc1a6b7951c79
小赵同学 <94612053+imzql@users.noreply.github.com>
2024-09-15 21:31:40 +0800

    修复所有bug

--- src/App.vue
+++ src/App.vue
@@ -25,9 +25,9 @@ onMounted(() => {
 // 响应式变量
 const options = ref([
   { label: '虾米解析', value: 'https://jx.xmflv.com/?url=' },
-  { label: '待添加', value: 'express' },
-  { label: '待添加', value: 'nextDay' },
-  { label: '待添加', value: 'sameDay' }
+  { label: '八戒解析', value: 'https://jx.m3u8.tv/jiexi/?url=' },
+  { label: '唐僧解析', value: 'https://jx.xmflv.cc/?url=' },
+  { label: '悟空解析', value: 'https://bd.jx.cn/?url=' }
 ]);
 
 const selectedShipping = ref('');
@@ -41,19 +41,26 @@ const updateIframeUrl = () => {
   if (jxUrl.value && videoUrl.value) {
     iframeUrl.value = `${jxUrl.value}${videoUrl.value}`;
   } else {
-    iframeUrl.value = '';
+    iframeUrl.value = options.value[0].value;
   }
 };
 
 // 处理播放请求
 const handleRequest = () => {
   updateIframeUrl();
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve();
-      LewMessage.success('解析成功');
-    }, 1000);
-  });
+  if (iframeUrl.value != options.value[0].value) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+        LewMessage.success('解析成功');
+      }, 1000);
+    });
+  } else {
+    LewMessage.error({
+    content: '错误：请先填写视频链接再提交,谢谢合作！',
+    duration: 3000
+  })
+  }
 };
 
 //抽屉
@@ -68,59 +75,59 @@ const openNewTab = (val) => {
 </script>
 
 <template>
-  <div id="content" >
-  <div class="head">
-    <span style="text-align: center;margin-bottom: 10px;">Vip免费视频解析</span>
-    <lew-badge value="important!" round color="red" style="display: inline-block;">
-      <lew-button color="black" size="medium" text="点我查看教程" dashed type="ghost" @click="openJC" />
-    </lew-badge>
-  </div>
-  <!-- 抽屉 -->
-  <lew-flex gap="20" x="start">
-    <lew-drawer title="使用教程" close-by-esc v-model:visible="visible" position="left" hideFooter="true">
-      <div class="jc-text">
-        <h2>操作指南：</h2>
-        <h3>第一步：</h3>
-        <p>这里以腾讯视频为例，腾讯视频：<a href="https://v.qq.com/channel/movie">点我跳转腾讯视频</a></p>
-        <p>找到想要观看的VIP视频</p>
-        <p><img src="https://ice.frostsky.com/2024/09/15/8fd7e548c47cbf69479ad3ea3f03e3d4.png" alt=""></p>
-        <h3>第二步：</h3>
-        <p>将浏览器上方的网址链接复制一下</p>
-        <p><img src="https://ice.frostsky.com/2024/09/15/74f1b4483de8acc98230c4d53dbbac4c.png" alt=""></p>
-        <p>将复制的链接粘贴到本站播放地址，并点击播放按钮</p>
-        <p><img src="https://ice.frostsky.com/2024/09/15/f60383ddf991d44ca2899d47d85a5830.png" alt=""></p>
-        <p>等待解析成功提示，即可免费观看VIP视频</p>
-      </div>
-    </lew-drawer>
-  </lew-flex>
-  <!-- 搜索栏 -->
-  <div class="nav">
-    <div class="route">
-      <lew-select v-model="jxUrl" clearable size="medium" placeholder="选择解析方式" :options="options" />
+  <div id="content">
+    <div class="head">
+      <span style="text-align: center;margin-bottom: 10px;">Vip免费视频解析</span>
+      <lew-badge value="important!" round color="red" style="display: inline-block;">
+        <lew-button color="black" size="medium" text="点我查看教程" dashed type="ghost" @click="openJC" />
+      </lew-badge>
     </div>
-    <lew-input v-model="videoUrl" align="left" placeholder="请键入视频链接" class="search" selectByFocus="true" />
-    <lew-button class="click" :request="handleRequest" text="播放" type="light" color="var(--lew-text-color-1);" />
-    <lew-button class="click clear" text="清空" type="light" color="var(--lew-text-color-1);"
-      @click="videoUrl = ''; iframeUrl = '';" />
-  </div>
-
-  <!-- 播放器 -->
-  <iframe :src="iframeUrl" class="player" allowfullscreen allowtransparency></iframe>
-
-  <div class="footer">
-    <lew-flex direction="y" gap="20">
-      <lew-flex wrap x="center" gap="10">
-        <lew-button size="medium" @click="openNewTab('https://v.qq.com')" text="进入腾讯视频" color="black" dashed
-          type="ghost" />
-        <lew-button size="medium" @click="openNewTab('https://www.iqiyi.com')" text="进入爱奇艺视频" color="black" dashed
-          type="ghost" />
-        <lew-button size="medium" @click="openNewTab('https://youku.com')" text="进入优酷视频" color="black" dashed
-          type="ghost" />
-        <lew-button size="medium" @click="openNewTab('https://www.mgtv.com')" text="进入芒果视频" color="black" dashed
-          type="ghost" />
-      </lew-flex>
+    <!-- 抽屉 -->
+    <lew-flex gap="20" x="start">
+      <lew-drawer title="使用教程" close-by-esc v-model:visible="visible" position="left" hideFooter="true">
+        <div class="jc-text">
+          <h2>操作指南：</h2>
+          <h3>第一步：</h3>
+          <p>这里以腾讯视频为例，腾讯视频：<a href="https://v.qq.com/channel/movie">点我跳转腾讯视频</a></p>
+          <p>找到想要观看的VIP视频</p>
+          <p><img src="https://ice.frostsky.com/2024/09/15/8fd7e548c47cbf69479ad3ea3f03e3d4.png" alt=""></p>
+          <h3>第二步：</h3>
+          <p>将浏览器上方的网址链接复制一下</p>
+          <p><img src="https://ice.frostsky.com/2024/09/15/74f1b4483de8acc98230c4d53dbbac4c.png" alt=""></p>
+          <p>将复制的链接粘贴到本站播放地址，并点击播放按钮</p>
+          <p><img src="https://ice.frostsky.com/2024/09/15/f60383ddf991d44ca2899d47d85a5830.png" alt=""></p>
+          <p>等待解析成功提示，即可免费观看VIP视频</p>
+        </div>
+      </lew-drawer>
     </lew-flex>
-  </div>
+    <!-- 搜索栏 -->
+    <div class="nav">
+      <div class="route">
+        <lew-select v-model="jxUrl" clearable size="medium" placeholder="选择解析方式" :options="options" />
+      </div>
+      <lew-input v-model="videoUrl" align="left" placeholder="请键入视频链接" class="search" selectByFocus="true" />
+      <lew-button class="click" :request="handleRequest" text="播放" type="light" color="var(--lew-text-color-1);" />
+      <lew-button class="click clear" text="清空" type="light" color="var(--lew-text-color-1);"
+        @click="videoUrl = ''; iframeUrl = '';" />
+    </div>
+
+    <!-- 播放器 -->
+    <iframe :src="iframeUrl" class="player" allowfullscreen allowtransparency></iframe>
+
+    <div class="footer">
+      <lew-flex direction="y" gap="20">
+        <lew-flex wrap x="center" gap="10">
+          <lew-button size="medium" @click="openNewTab('https://v.qq.com')" text="进入腾讯视频" color="black" dashed
+            type="ghost" />
+          <lew-button size="medium" @click="openNewTab('https://www.iqiyi.com')" text="进入爱奇艺视频" color="black" dashed
+            type="ghost" />
+          <lew-button size="medium" @click="openNewTab('https://youku.com')" text="进入优酷视频" color="black" dashed
+            type="ghost" />
+          <lew-button size="medium" @click="openNewTab('https://www.mgtv.com')" text="进入芒果视频" color="black" dashed
+            type="ghost" />
+        </lew-flex>
+      </lew-flex>
+    </div>
   </div>
 </template>
 
